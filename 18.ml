7e89e254724f22a07ba8e0c79f373b1402a598b3
yzernik <yzernik@gmail.com>
2014-10-03 23:04:12 -0700

    solved 18

---
+++ 18.ml
@@ -0,0 +1,26 @@
+(*
+ * Extract a slice from a list.
+ *)
+
+let slice xs n m =
+  let rec slice_helper acc hxs hn hm =
+    if (hm < 0) then
+      acc
+    else
+      match hxs with
+      [] -> acc
+    | h::t ->
+       let newAcc = 
+	 if (hn <= 0) then
+	   acc @ [h]
+	 else
+	   acc
+       in
+       slice_helper newAcc t (hn-1) (hm-1)
+  in
+  slice_helper [] xs n m
+
+
+;;
+
+assert (slice ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 2 6 = ["c"; "d"; "e"; "f"; "g"]) ;;
