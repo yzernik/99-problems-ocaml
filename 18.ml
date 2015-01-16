15025011d8d00a7a96c29bdd2b37ee52f6301fea
yzernik <yzernik@gmail.com>
2015-01-15 20:07:02 -0800

    fixed indent

--- 18.ml
+++ 18.ml
@@ -8,19 +8,17 @@ let slice xs n m =
       acc
     else
       match hxs with
-      [] -> acc
-    | h::t ->
-       let newAcc = 
-	 if (hn <= 0) then
-	   acc @ [h]
-	 else
-	   acc
-       in
-       slice_helper newAcc t (hn-1) (hm-1)
+	[] -> acc
+      | h::t ->
+	 let newAcc = 
+	   if (hn <= 0) then
+	     acc @ [h]
+	   else
+	     acc
+	 in
+	 slice_helper newAcc t (hn-1) (hm-1)
   in
   slice_helper [] xs n m
-
-
 ;;
 
 assert (slice ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 2 6 = ["c"; "d"; "e"; "f"; "g"]) ;;
